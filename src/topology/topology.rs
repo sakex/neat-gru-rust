c623786cbff21c1ae352d8ec38c833561629c92d
sakex <alexandre@senges.ch>
2021-01-10 17:30:15 +0100

    A bit more species late game

--- src/topology/topology.rs
+++ src/topology/topology.rs
@@ -142,7 +142,7 @@ where
             one
         };
         disjoints = disjoints + size_1 - common;
-        2 * disjoints / n + w * 2
+        3 * disjoints / n + w * 3
     }
 
     pub fn new_random(
